559350becdc5216267eefeb09961859c6cfbccc5
Venia <vsollery@gmail.com>
2022-12-02 00:00:09 +0700

    try with iphone 4

--- public/main.js
+++ public/main.js
@@ -1,24 +1,206 @@
-import * as THREE from 'three';
+const colors = [
+    {
+        color: '66533C'
+    },
+    {
+        color: '173A2F'
+    },
+    {
+        color: '153944'
+    },
+    {
+        color: '27548D'
+    },
+    {
+        color: '438AAC'
+    }  
+]
+
+const canvas = document.querySelector('#webgl');
+
+var theModel;
+const MODEL_PATH = "/src/assets/Iphone-4/scene.gltf";
+
+const BACKGROUND_COLOR = 0xf1f1f1;
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+scene.background = new THREE.Color(BACKGROUND_COLOR );
+scene.fog = new THREE.Fog(BACKGROUND_COLOR, 20, 100);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
-camera.position.z = 5;
+var cameraFar = 20;
+
+const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
+
+renderer.shadowMap.enabled = true;
+renderer.setPixelRatio(window.devicePixelRatio); 
+
+document.body.appendChild(renderer.domElement);
+
+var camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000);
+camera.position.z = cameraFar;
+camera.position.x = 0;
+camera.position.y = 10;
+
+
+const INITIAL_MTL = new THREE.MeshPhongMaterial( { color: 0xf1f1f1, shininess: 10 } );
+
+const INITIAL_MAP = [
+    {childID: "camera ring", mtl: INITIAL_MTL},
+];
+
+
+// Init the object loader
+var loader = new THREE.GLTFLoader();
+
+loader.load(MODEL_PATH, function(gltf) {
+  theModel = gltf.scene;
+
+  theModel.traverse((o) => {
+    if (o.isMesh) {
+      o.castShadow = true;
+      o.receiveShadow = true;
+    }
+  });
+
+// Set the models initial scale   
+  theModel.scale.set(2,2,2);
+  // Offset the y position a bit
+  theModel.position.y = Math.PI;
+
+  for (let object of INITIAL_MAP) {
+    initColor(theModel, object.childID, object.mtl);
+  }
+// Add the model to the scene
+  scene.add(theModel);
+}, undefined, function(error) {
+  console.error(error)
+});
+
+function initColor(parent, type, mtl) {
+    parent.traverse(o => {
+        if (o.isMesh) {
+            if (o.name.includes(type)) {
+                o.material = mtl;
+                o.nameID = type; // Set a new property to identify this object
+            }
+        }
+    });
+}
+
+// Add lights
+var ambLight = new THREE.AmbientLight(0x404040, 1);
+scene.add(ambLight);
+
+var dirLight = new THREE.DirectionalLight( 0xffffff, 1);
+dirLight.position.set( -8, 10, 20);
+dirLight.castShadow = true;
+scene.add(dirLight);
+
+// const dirHelper = new THREE.DirectionalLightHelper(dirLight, 3)
+// scene.add(dirLight, dirHelper);
+
+//point light 1
+var pointLight = new THREE.PointLight(0xffffff, 1.5);
+pointLight.position.set(2, 50, 500);
+scene.add(pointLight);
+
+const sphereSize = 10;
+const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
+scene.add( pointLightHelper );
+
+//point light 2
+var pointLight2 = new THREE.PointLight(0xc4c4c4,1.5);
+pointLight2.position.set(500, 100, 0);
+scene.add(pointLight2);
+
+const sphereSize2 = 10;
+const pointLightHelper2 = new THREE.PointLightHelper( pointLight2, sphereSize2 );
+scene.add( pointLightHelper2 );
+
+//point light 3
+var pointLight3 = new THREE.PointLight(0xc4c4c4,1.5);
+pointLight3.position.set(0, 100, -500);
+scene.add(pointLight3);
+
+const sphereSize3 = 1;
+const pointLightHelper3 = new THREE.PointLightHelper( pointLight3, sphereSize3 );
+scene.add( pointLightHelper3 );
+
+//point light 4
+var pointLight4 = new THREE.PointLight(0xc4c4c4,1.5);
+pointLight4.position.set(-500, 300, 0);
+scene.add(pointLight4);
+
+const sphereSize4 = 10;
+const pointLightHelper4 = new THREE.PointLightHelper( pointLight4, sphereSize4 );
+scene.add( pointLightHelper4 );
+
+var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.5 );
+    hemiLight.position.set( 0, 50, 0 );
+// Add hemisphere light to scene   
+scene.add( hemiLight );
+  
+
+ 
+//floor
+var floorGeometry = new THREE.PlaneGeometry(500,500,32);
+var floorMaterial = new THREE.MeshPhongMaterial({
+  color:  '#52595D',
+  depthWrite: false,
+  shininess: 0
+});
+
+var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.rotation.x =  -0.5 * Math.PI;
+floor.receiveShadow = true;
+floor.position.y = -2;
+scene.add(floor);
+
+
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.maxPolarAngle = Math.PI;
+controls.minPolarAngle = - Math.PI;
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.dampingFactor = 0.1;
+controls.autoRotate = true; // Toggle this if you'd like the chair to automatically rotate
+controls.autoRotateSpeed = false; // 30
 
 function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+    controls.update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+
+    if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }
 }
+
 animate();
 
-cube.rotation.x += 0.01;
-cube.rotation.y += 0.01;
+function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    var width = sizes.width;
+    var height = sizes.height;
+    var canvasPixelWidth = canvas.width / window.devicePixelRatio;
+    var canvasPixelHeight = canvas.height / window.devicePixelRatio;
+    
+    const needResize = canvasPixelWidth !== width || canvasPixelHeight !== height;
+    if (needResize) {
+        
+        renderer.setSize(width, height, false);
+    }
+    return needResize;
+}
+
+
+
+
